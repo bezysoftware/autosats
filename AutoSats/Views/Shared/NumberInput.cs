5b2ac86f86688c6ec0c89f8e56a53814af19c816
Tomas Bezouska <bezysoftware@outlook.com>
2021-09-10 14:48:17 +0200

    Number input

---
+++ AutoSats/Views/Shared/NumberInput.cs
@@ -0,0 +1,25 @@
+﻿using AntDesign;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace AutoSats.Views.Shared
+{
+    /// <summary>
+    /// Force a number input to be on en-US culture (dot as decimal separator).
+    /// </summary>
+    public class NumberInput<TValue> : Input<TValue?>
+    {
+        public NumberInput()
+        {
+            CultureInfo = new System.Globalization.CultureInfo("en-US");
+        }
+
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            // temporarily switch to en-US culture to enforce dot as decimal separator when building the render tree
+            var culture = System.Threading.Thread.CurrentThread.CurrentCulture;
+            System.Threading.Thread.CurrentThread.CurrentCulture = CultureInfo;
+            base.BuildRenderTree(builder);
+            System.Threading.Thread.CurrentThread.CurrentCulture = culture;
+        }
+    }
+}
