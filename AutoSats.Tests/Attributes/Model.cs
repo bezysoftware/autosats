67c280e5a1ada7052140215ec7dfc9eccf10cfd3
Tomas Bezouska <bezysoftware@outlook.com>
2021-09-20 15:10:04 +0200

    BitcoinAddress attribute test

---
+++ AutoSats.Tests/Attributes/Model.cs
@@ -0,0 +1,10 @@
+﻿using AutoSats.Validation;
+
+namespace AutoSats.Tests.Attributes
+{
+    public class Model
+    {
+        [BitcoinAddress]
+        public string Address { get; set; }
+    }
+}
