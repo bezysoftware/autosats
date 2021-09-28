a5823e7603a8d28c6f9ccd7f23411b30409837db
Tomas Bezouska <bezysoftware@outlook.com>
2021-09-28 21:04:30 +0200

    Load or created wallet if none loaded

--- AutoSats/Execution/Services/WalletService.cs
+++ AutoSats/Execution/Services/WalletService.cs
@@ -1,4 +1,7 @@
 ﻿using NBitcoin.RPC;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace AutoSats.Execution.Services
@@ -14,10 +17,48 @@ namespace AutoSats.Execution.Services
 
         public async Task<string> GenerateDepositAddressAsync()
         {
+            try
+            {
+                return await GetNewAddressAsync();
+            }
+            catch (RPCException ex) when (ex.RPCCode == RPCErrorCode.RPC_WALLET_NOT_FOUND)
+            {
+                await LoadOrCreateWalletAsync();
+                return await GetNewAddressAsync();
+            }
+        }
+
+        private async Task LoadOrCreateWalletAsync()
+        {
+            var response = await this.client.SendCommandAsync("listwalletdir");
+            var wallets = JsonConvert.DeserializeObject<WalletsResponse>(response.ResultString);
+
+            if (wallets.Wallets.Any())
+            {
+                await this.client.LoadWalletAsync(wallets.Wallets[0].Name);
+            }
+            else
+            {
+                await this.client.CreateWalletAsync("autosats");
+            }
+        }
+
+        private async Task<string> GetNewAddressAsync()
+        {
             var request = new GetNewAddressRequest { Label = "AutoSats" };
             var address = await this.client.GetNewAddressAsync(request);
-
             return address.ToString();
+        }
+
+        internal class WalletsResponse
+        {
+            [JsonProperty("wallets")]
+            public WalletResponse[] Wallets { get; set; } = Array.Empty<WalletResponse>();
+        }
+
+        internal class WalletResponse
+        {
+            public string Name { get; set; } = string.Empty;
         }
     }
 }
