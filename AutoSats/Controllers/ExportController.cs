ca3075839285a0fd1f078cff2c008c853eb5e65d
Tomas Bezouska <bezysoftware@outlook.com>
2022-01-19 23:28:24 +0100

    feature(export): csv export

---
+++ AutoSats/Controllers/ExportController.cs
@@ -0,0 +1,39 @@
+﻿using AutoSats.Execution;
+using AutoSats.Models;
+using CsvHelper;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+using System.Text;
+
+namespace AutoSats.Controllers;
+
+[ApiController]
+[Route("api/export")]
+public class ExportController : ControllerBase
+{
+    private readonly IExchangeScheduler scheduler;
+
+    public ExportController(IExchangeScheduler scheduler)
+    {
+        this.scheduler = scheduler;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> ExportAsync(int id)
+    {
+        var events = await this.scheduler.GetScheduleEventsAsync(id);
+
+        using (var writer = new StringWriter())
+        using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+        {
+            csv.Context.RegisterClassMap<ExchangeEventMap>();
+
+            csv.WriteRecords(events.Select(x => new BuyWithdrawal(x)));
+
+            var contentType = "text/csv";
+            var fileName = "export.csv";
+
+            return File(Encoding.UTF8.GetBytes(writer.ToString()), contentType, fileName);
+        }
+    }
+}
---
+++ AutoSats/Models/ExchangeEventMap.cs
@@ -0,0 +1,18 @@
+﻿using CsvHelper.Configuration;
+
+namespace AutoSats.Models;
+
+public class ExchangeEventMap : ClassMap<BuyWithdrawal>
+{
+    public ExchangeEventMap()
+    {
+        Map(x => x.Timestamp).TypeConverterOption.Format("o").Index(0);
+        Map(x => x.Type).Index(1);
+        Map(x => x.Buy!.OrderId).Index(2);
+        Map(x => x.Buy!.Price).Index(3);
+        Map(x => x.Buy!.Received).Index(4);
+        Map(x => x.Withdrawal!.WithdrawalId).Index(5);
+        Map(x => x.Withdrawal!.Address).Index(6).Name("WithdrawalAddress");
+        Map(x => x.Withdrawal!.Amount).Index(7).Name("WithdrawalAmount");
+    }
+}
